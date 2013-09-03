39c67e679468fe6aed76930b25ed6eb91d1e0a2a
Andrei Puni <andrei.puni@yardi.com>
2013-09-03 18:59:26 +0300

    updated update.sh script

--- update.sh
+++ update.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
-git pull CocoaPods master
+#!/bin/bash
+pushd /Users/andrei/projects/Specs/cocoapods
+
+echo -e "\n\n===================================\n\n"
+git pull CocoaPods master 
 git push origin master
+echo -e "\n ..updated cocoapods on `date`" 
+say -v Zarvox Cocoa pods updated
+
+popd
+
